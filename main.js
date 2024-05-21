1e1ee93e6b676373600d293b59825daea6403f18
Your Name <you@example.com>
2024-05-21 13:07:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let a = prompt('7 gacha son')
+
+switch (a) {
+    case '1':
+        alert('Dushanba')
+        break;
+    case '2':
+        alert('Seshanba')
+        break;
+    case '3':
+        alert('Chorshanba')
+        break;
+    case '4':
+        alert('Payshanba')
+        break;
+    case '5':
+        alert('juma')
+        break;
+    case '6':
+        alert('Shanba')
+        break;
+    case '7':
+        alert('Yakshanba')
+        break;
+        default:alert('bunday kun yoq')
+}
